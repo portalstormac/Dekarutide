63736e26f88bb1c9a531ff9332a27fe45216dad9
Dekaru <dekaru@protonmail.com>
2022-07-17 16:45:05 -0300

    Updated player movement enforcement code.

--- Source/ACE.Server/WorldObjects/Player_Tick.cs
+++ Source/ACE.Server/WorldObjects/Player_Tick.cs
@@ -444,6 +444,7 @@ namespace ACE.Server.WorldObjects
                                 float currentMaxSpeed;
                                 float timeSinceLastAction;
                                 bool isMovingOrAnimating;
+
                                 if (FastTick && velocity != 0.0f)
                                 {
                                     if (PhysicsObj.IsMovingOrAnimating || IsMoving || IsPlayerMovingTo || IsPlayerMovingTo2)
@@ -462,8 +463,10 @@ namespace ACE.Server.WorldObjects
                                 }
                                 else
                                 {
+                                    var isWaitingForNextUseTime = DateTime.UtcNow < NextUseTime;
                                     var isPlayerInitiatedMovement = (CurrentMoveToState.RawMotionState.Flags & (RawMotionFlags.ForwardCommand | RawMotionFlags.SideStepCommand | RawMotionFlags.TurnCommand)) != 0;
-                                    if (isPlayerInitiatedMovement || IsJumping || IsMoving || IsPlayerMovingTo || IsPlayerMovingTo2)
+
+                                    if (isPlayerInitiatedMovement || IsJumping || IsMoving || IsPlayerMovingTo || IsPlayerMovingTo2 || isWaitingForNextUseTime)
                                         LastPlayerInitiatedActionTime = DateTime.UtcNow;
 
                                     timeSinceLastAction = (float)(DateTime.UtcNow - LastPlayerInitiatedActionTime).TotalSeconds;
