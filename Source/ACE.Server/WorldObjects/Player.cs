fa885c01dbf9d95c638d29793691ccd3e70cea06
Dekaru <dekaru@protonmail.com>
2022-07-18 09:38:57 -0300

    Updated player movement enforcement code again, getting there!

--- Source/ACE.Server/WorldObjects/Player.cs
+++ Source/ACE.Server/WorldObjects/Player.cs
@@ -77,7 +77,9 @@ namespace ACE.Server.WorldObjects
         public ACE.Entity.Position LastGroundPos;
         public ACE.Entity.Position SnapPos;
         public float PrevMovementUpdateMaxSpeed;
+        public bool HasPerformedActionsSinceLastMovementUpdate; // Ideally we wouldn't need this, but necessary until we figure out how to detect certain actions while a player isn't running with FastTicks enabled.
         public DateTime LastPlayerInitiatedActionTime;
+        public DateTime LastPlayerMovementCheckTime;
 
         public ConfirmationManager ConfirmationManager;
 
@@ -963,6 +965,7 @@ namespace ACE.Server.WorldObjects
             }*/
 
             LastJumpTime = DateTime.UtcNow;
+            HasPerformedActionsSinceLastMovementUpdate = true;
 
             UpdateVitalDelta(Stamina, -staminaCost);
 
