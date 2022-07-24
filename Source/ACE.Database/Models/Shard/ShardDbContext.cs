b2ba15bb6d2ac47d68be1df6b2581f3f893f35e1
Dekaru <dekaru@protonmail.com>
2022-07-24 20:54:23 -0300

    Merging latest ACE version.

--- Source/ACE.Database/Models/Shard/ShardDbContext.cs
+++ Source/ACE.Database/Models/Shard/ShardDbContext.cs
@@ -68,7 +68,7 @@ namespace ACE.Database.Models.Shard
             {
                 var config = Common.ConfigManager.Config.MySql.Shard;
 
-                var connectionString = $"server={config.Host};port={config.Port};user={config.Username};password={config.Password};database={config.Database};TreatTinyAsBoolean=False";
+                var connectionString = $"server={config.Host};port={config.Port};user={config.Username};password={config.Password};database={config.Database};TreatTinyAsBoolean=False;SslMode=None;AllowPublicKeyRetrieval=true;ApplicationName=ACEmulator";
 
                 optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), builder =>
                 {
