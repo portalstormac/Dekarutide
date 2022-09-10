48a5799086e7e61df1a3dd81ce68a1ab715511a6
Dekaru <dekaru@protonmail.com>
2022-09-10 08:38:44 -0300

    - CustomDM: Training Academy revamped to be shorter while keeping the same lessons and improving secondary rewards a little.

--- Source/ACE.Server/Factories/PlayerFactory.cs
+++ Source/ACE.Server/Factories/PlayerFactory.cs
@@ -470,7 +470,7 @@ namespace ACE.Server.Factories
                 var starterAreaName = starterArea.Name;
                 if (starterAreaName != "Shoushi" && starterAreaName != "Yaraq" && starterAreaName != "Holtburg")
                 {
-                    switch(ThreadSafeRandom.Next(1, 3))
+                    switch (ThreadSafeRandom.Next(1, 3))
                     {
                         case 1:
                             starterAreaName = "Shoushi";
@@ -483,17 +483,26 @@ namespace ACE.Server.Factories
                             break;
                     }
                 }
-                switch (starterAreaName)
+                switch (starterArea.Name)
                 {
                     case "Shoushi":
-                        player.Location = new Position(0xDE51001D, 87.350517f, 114.857246f, 16.004999f, 0, 0, 0.537665f, 0.843159f);
+                        if(ThreadSafeRandom.Next(0, 1) == 1)
+                            player.Location = new Position(0xD6550023, 108.765625f, 62.215103f, 52.005001f, 0.000000f, 0.000000f, -0.300088f, 0.953912f); // Shoushi West
+                        else
+                            player.Location = new Position(0xDE51001D, 85.017159f, 107.291908f, 15.861228f, 0.000000f, 0.000000f, 0.323746f, 0.946144f); // Shoushi Southeast
                         break;
                     case "Yaraq":
-                        player.Location = new Position(0x7D680012, 56.949219f, 29.860384f, 14.981730f, 0, 0, -0.879578f, 0.475755f);
+                        if (ThreadSafeRandom.Next(0, 1) == 1)
+                            player.Location = new Position(0x7D680012, 65.508179f, 37.516647f, 16.257774f, 0.000000f, 0.000000f, -0.950714f, 0.310069f); // Yaraq North
+                        else
+                            player.Location = new Position(0x8164000D, 40.296101f, 107.638382f, 31.363008f, 0.000000f, 0.000000f, -0.699884f, -0.714257f); //Yaraq East
                         break;
                     case "Holtburg":
                     default:
-                        player.Location = new Position(0xA9B00015, 53.780079f, 105.814995f, 64.005005f, 0, 0, -0.359575f, -0.933116f);
+                        if (ThreadSafeRandom.Next(0, 1) == 1)
+                            player.Location = new Position(0xA5B4002A, 131.134338f, 33.602352f, 53.077141f, 0.000000f, 0.000000f, -0.263666f, 0.964614f); // Holtburg West
+                        else
+                            player.Location = new Position(0xA9B00015, 60.108139f, 103.333549f, 64.402885f, 0.000000f, 0.000000f, -0.381155f, -0.924511f); // Holtburg South
                         break;
                 }
 
