2ec06139ca77cf12a5e311e417fc733748051a18
Dekaru <dekaru@protonmail.com>
2022-11-15 18:11:36 -0300

    CustomDM: Magic gems now have a suffix indicating which spell they cast.

--- Source/ACE.Server/Factories/LootGenerationFactory_Gem.cs
+++ Source/ACE.Server/Factories/LootGenerationFactory_Gem.cs
@@ -69,6 +69,8 @@ namespace ACE.Server.Factories
 
             // long desc
             wo.LongDesc = GetLongDesc(wo);
+            if (Common.ConfigManager.Config.Server.WorldRuleset == Common.Ruleset.CustomDM && isMagical && wo.LongDesc != wo.Name)
+                wo.Name = GetLongDesc(wo);
         }
 
         private static void AssignMagic_Gem(WorldObject wo, TreasureDeath profile)
