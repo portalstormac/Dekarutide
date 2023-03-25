9f47d238324197faa092bf5ed260bc227e5d3c06
portalstormac <128569743+portalstormac@users.noreply.github.com>
2023-03-24 21:35:24 -0500

    Update CampManager.cs

--- Source/ACE.Server/Managers/CampManager.cs
+++ Source/ACE.Server/Managers/CampManager.cs
@@ -255,9 +255,9 @@ namespace ACE.Server.Managers
             }
         }
 
-        public static uint MaxInteractionsRestCamp = 500;
-        public static uint MaxInteractionsAreaCamp = 750;
-        public static uint MaxInteractionsTypeCamp = 2000;
+        public static uint MaxInteractionsRestCamp = 1000;
+        public static uint MaxInteractionsAreaCamp = 2000;
+        public static uint MaxInteractionsTypeCamp = 3000;
         public static uint MaxInteractionsTurninTypeCamp = 10;
 
         // When changing these values remember to also update the values in ShardDatabaseOfflineTools.cs
